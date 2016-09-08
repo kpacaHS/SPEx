626d2054ba161bfc8942956490f560654abf608c
krispaks <krispaks@yahoo.com>
2016-09-07 23:41:22 -0500

    experiment stuff

--- app/components/shared/calendar-modal/calendar-modal.component.js
+++ app/components/shared/calendar-modal/calendar-modal.component.js
@@ -2,7 +2,7 @@ import CalendarModalController from './calendar-modal.controller';
 export default {
     name: 'calendarModalComponent',
     config: {
-        template: '<div></div>',
+        template: '<div><md-button aria-label="Eat cake" ng-click=\'$ctrl.doSomething()\'>Component</md-button></div>',
         controller: ['$mdDialog', CalendarModalController],
         bindings: {
             option: '<',
--- app/components/shared/calendar-modal/calendar-modal.controller.js
+++ app/components/shared/calendar-modal/calendar-modal.controller.js
@@ -1,3 +1,9 @@
+class TestController{
+    constructor(){
+
+    }
+}
+
 class CalendarModalController{    
     constructor($mdDialog){
         this.$mdDialog = $mdDialog;
@@ -5,21 +11,32 @@ class CalendarModalController{
     }
 
     showDialog(){
-        var confirm = this.$mdDialog.confirm()
+        /*var confirm = this.$mdDialog.confirm()
           .title('Would you like to delete your debt?')
           .textContent('All of the banks have agreed to forgive you your debts.')
           .ariaLabel('Lucky day')
           .targetEvent({})
           .ok('Please do it!')
-          .cancel('Sounds like a scam');
+          .cancel('Sounds like a scam');*/
 
-        this.$mdDialog.show(confirm)
-            .then(() => {
-                console.log('dodo ng cow1');                    
-            },
-            () => {
-                console.log('dodo ng cow2');
-            });
+        this.$mdDialog.show({
+            controller: TestController,
+            template: '<md-dialog>Testing <md-button aria-label="Eat cake" ng-click=\'$ctrl.doSomething()\'>Dialog</md-button></md-dialog>',
+            parent: angular.element(document.body),
+            targetEvent: {},
+            clickOutsideToClose: true
+        })
+        .then(() => {
+            console.log('dodo ng cow1');                    
+        },
+        () => {
+            console.log('dodo ng cow2');
+        });
+    }
+
+    doSomething()
+    {
+        console.log('dodo ng cow');
     }    
 }
 
